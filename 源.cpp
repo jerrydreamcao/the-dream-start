f63909ab8b52581ae2c1d5e902aa8b01177353dc
jerrydreamcao <cao20061017@outlook.com>
2024-09-21 20:12:04 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 1;
+//	int sum = 1;
+//	scanf("%d %d", &a,&b);
+//	sum = a + b;
+//	printf("sum=%d\n", sum);
+//	return 0;
+//}
+int main()
+{
+	printf("hehe\n");
+	return 0;
+}
