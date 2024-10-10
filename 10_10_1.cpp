09b552c939564a69917cdc48e5a7f16f05644dee
jerrydreamcao <cao20061017@outlook.com>
2024-10-10 17:45:26 +0800

    Add files via upload

---
+++ 10_10_1.cpp
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int main()
+{
+	//int a = 9 / 2;
+	//float a = 9 / 2.0;
+	//int b = 9 % 2;// % 取余
+
+	//printf("%f\n", a);
+	//printf("%d\n", b);
+
+	//int a = 2;
+	//int b = a << 1;
+	//printf("%d\n", b);
+	//int a = 2;
+	//a += 5;// 等价于 a=a+5
+	//printf("%d\n", a);
+
+
+	//int a = 0;
+	//printf("%d\n", !a);
+
+	//if (a)
+	//{
+
+	//}
+	//if (!a)
+	//{
+
+	//}
+
+
+	int arr[10] = { 0 };
+	int a = 10;
+	printf("%d\n", sizeof(int));//(int)的括号不能去掉
+	printf("%d\n", sizeof(a));//(a)的括号可以去掉
+	printf("%d\n", sizeof(arr));
+	
+
+
+
+
+
+
+
+
+	return 0;
+}
