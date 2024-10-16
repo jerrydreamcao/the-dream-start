bd2651d0a89f101e9ca9f0bdb473eca6abc98691
jerrydreamcao <cao20061017@outlook.com>
2024-10-16 17:18:48 +0800

    Add files via upload

---
+++ 10_16.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+//int main()
+//{
+//	{
+//		int a = 1;//自动创建，自动销毁 - 自动变量 = auto int a=1;
+//	}
+//
+//
+//	return 0;
+//}
+
+int main()
+{
+	register int a = 1;//建议a的值存放在寄存器中
+
+	return 0;
+}
