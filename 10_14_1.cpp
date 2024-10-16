0555bb93387470c2260bf2be80b39717a366d018
jerrydreamcao <cao20061017@outlook.com>
2024-10-16 09:46:21 +0800

    Add files via upload

---
+++ 10_14_1.cpp
@@ -0,0 +1,89 @@
+#include<stdio.h>
+//int main()
+//{
+//	int a = -1;
+//	printf("%d\n", ~a);
+//
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int a = 10;
+//	int b = a++;
+//
+//	printf("%d\n", b);
+//	printf("%d\n", a);
+//
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 3.14;
+//	printf("%d\n", a);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 4;
+//	int c = a && b;
+//
+//	printf("%d\n", c);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 2;
+//	int b = 3;
+//	int max1 = 0;
+//	int max2 = 0;
+//
+//	if (a > b)
+//		max1 = a;
+//	else
+//		max1 = b;
+//
+//	max2 = a > b ? a : b;
+//
+//	printf("%d\n", max1);
+//	printf("%d\n", max2);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 4;
+//	int b = 5;
+//	int c = 7;
+//	int d = (a = b + 1, c = a - 1, b = c + 8);
+//	printf("%d\n", d);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	printf("%d\n", arr[6]);
+//
+//	return 0;
+//}
+
+int main()
+{
+	printf("hehe\n");//一个操作数
+	printf("%d\n",100);//两个操作数
+
+	return 0;
+ }
