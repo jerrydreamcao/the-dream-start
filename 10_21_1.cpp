8890aa1f216f8cd3a172765f87ceb6be76788425
jerrydreamcao <cao20061017@outlook.com>
2024-10-21 08:50:55 +0800

    Add files via upload

---
+++ 10_21_1.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+//#define MAX 100
+//
+//int main()
+//{
+//	printf("%d\n", MAX);
+//
+//	return 0;
+//}
+
+#define ADD(x,y) ((x)+(y))
+
+int main()
+{
+	printf("%d\n",2 * ADD(6, 7));
+	return 0;
+}
+
